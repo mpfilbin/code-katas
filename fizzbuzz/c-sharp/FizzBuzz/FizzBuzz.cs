bc72364ff83586920e7db7cece3c9e6c59e8b3a8
LAtheDeveloper <leila.n.adams@gmail.com>
2013-08-30 16:25:49 -0600

    Adds C# fizz buzz; updates to ignore Visual Studio files

---
+++ fizzbuzz/c-sharp/FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,67 @@
+﻿using NUnit.Framework;
+
+[TestFixture]
+public class FizzBuzz
+{
+    [Test]
+    public void PassingInOneShouldReturnOne()
+    {
+        var result = FizzBuzzCalc(1);
+        Assert.AreEqual("1", result);
+    }
+
+    [Test]
+    public void PassingInTwoShouldReturnTwo()
+    {
+        var result = FizzBuzzCalc(2);
+        Assert.AreEqual("2", result);
+    }
+
+    [Test]
+    public void PassingInThreeShouldReturnFizz()
+    {
+        var result = FizzBuzzCalc(3);
+        Assert.AreEqual("Fizz", result);
+    }
+
+    [Test]
+    public void PassingInSixShouldReturnFizz()
+    {
+        var result = FizzBuzzCalc(6);
+        Assert.AreEqual("Fizz", result);
+    }
+
+    [Test]
+    public void PassingInFiveShouldReturnBuzz()
+    {
+        var result = FizzBuzzCalc(5);
+        Assert.AreEqual("Buzz", result);
+    }
+
+    [Test]
+    public void PassingIn15ShouldReturnFizzBuzz()
+    {
+        var result = FizzBuzzCalc(15);
+        Assert.AreEqual("Fizz Buzz", result);
+    }
+
+    public string FizzBuzzCalc(int value)
+    {
+        var output = value.ToString();
+
+        if (value % 15 == 0)
+        {
+            output = "Fizz Buzz";
+        }
+        else if (value % 3 == 0)
+        {
+            output = "Fizz";
+        }
+        else if (value % 5 == 0)
+        {
+            output = "Buzz";
+        }
+
+        return output;
+    }
+}
---
+++ fizzbuzz/c-sharp/README.md
@@ -0,0 +1,10 @@
+# FizzBuzz C# Implementation
+
+This implementation of fizzbuzz was written in Visual Studio 2012 against .NET Framework 4.5. It uses NUnit, a third-party unit test dependency.
+
+If you do not have Visual Studio installed on your machine, you can download Visual Studio Express 2012 from Microsoft [http://www.microsoft.com/en-us/download/]. 
+
+To install NUnit, download and install NuGet package manager. Open the FizzBuzz project. Right-click on References > Manage NuGet Packages. Search for NUnit and click Install. 
+
+From Visual Studio main menu, navigate to Test > Run > All Tests.
+
